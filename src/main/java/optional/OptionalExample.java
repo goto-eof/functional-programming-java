7bbde688fac7b2a31fa5ed2bb82794c950133858
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-31 08:14:08 +0100

    wip

--- src/main/java/optional/OptionalExample.java
+++ src/main/java/optional/OptionalExample.java
@@ -17,6 +17,34 @@ public class OptionalExample {
         return Optional.empty();
     }
 
+
+    public static String orElse() {
+        Student student = null;
+        return Optional.ofNullable(student).map(Student::getName).orElse("NOT_FOUND");
+    }
+
+    public static String orElseGet() {
+        Student student = null;
+        return Optional.ofNullable(student).map(Student::getName).orElseGet(() -> "NOT_FOUND");
+    }
+
+    public static String orElseThrow() {
+        Student student = null;
+        return Optional.ofNullable(student).map(Student::getName).orElseThrow(() -> new RuntimeException("NOT_FOUND"));
+    }
+
+    public static void ifPresent() {
+        Student student = new Student();
+        Optional.ofNullable(student).ifPresent(stu -> System.out.println(stu));
+    }
+
+
+    public static void map() {
+        Optional<Student> opt = Optional.ofNullable(new Student());
+        System.out.println(opt.map(Student::getName));
+    }
+
+
     public static void main(String[] args) {
         Optional<Student> optionalStudent = Optional.ofNullable(null);
         if (optionalStudent.isPresent()) {
@@ -27,6 +55,13 @@ public class OptionalExample {
 
         System.out.println(getStudentName(null));
         System.out.println(getStudentName(Database.retrieveList().get(0)));
+
+        ifPresent();
+        map();
+        System.out.println(orElse());
+        System.out.println(orElseGet());
+        System.out.println(orElseThrow());
+
 
     }
 }
